a8b746e8a3ed2d8c8bada7c3dc4c1586827a158a
D0NIFF <abdulmalikgeraev59@gmail.com>
2024-10-03 06:56:13 +0300

    Create new migrations: auth_logs, user_confirms, user_notifications, complaint_statuses and complaints

---
+++ database/migrations/2024_10_03_031851_create_complaints_table.php
@@ -0,0 +1,43 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('complaints', function (Blueprint $table) {
+            $table->uuid('id')
+                ->primary();
+
+            $table->uuid('sender_id');
+            $table->uuid('offender_id');
+
+            $table->string('title', 100);
+            $table->text('description');
+
+            $table->jsonb('attachments');
+
+            $table->uuid('status_id');
+            $table->foreign('status_id')
+                ->references('id')
+                ->on('complaint_statuses');
+
+            $table->timestamps();
+            $table->softDeletes();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('complaints');
+    }
+};
