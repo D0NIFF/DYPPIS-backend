d65732e3740ab585efe3d0d0a251e77906f75506
D0NIFF <abdulmalikgeraev59@gmail.com>
2024-10-05 11:54:56 +0300

    Delete not used namespaces

--- app/Models/ProductService/Platform.php
+++ app/Models/ProductService/Platform.php
@@ -3,11 +3,8 @@
 namespace App\Models\ProductService;
 
 use App\Http\Requests\ProductService\IndexProductRequest;
-use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Relations\BelongsTo;
-use Illuminate\Database\Eloquent\Relations\HasMany;
-use Illuminate\Support\Facades\DB;
 
 class Platform extends Model
 {
