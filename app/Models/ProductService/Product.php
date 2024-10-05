25d35042673fd51bd5e619e62abdbd564381ecfe
D0NIFF <abdulmalikgeraev59@gmail.com>
2024-10-05 19:41:07 +0300

    Add new methods: getPlatform, getCategory, getDelivery, getDiscount

--- app/Models/ProductService/Product.php
+++ app/Models/ProductService/Product.php
@@ -2,9 +2,11 @@
 
 namespace App\Models\ProductService;
 
+use App\Http\Controllers\ProductService\ProductController;
 use App\Models\User;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
 
 class Product extends Model
 {
@@ -14,11 +16,12 @@ class Product extends Model
     public $incrementing = false;
 
     protected $fillable = [
-        'slug',
+        'id',
         'title',
         'description',
         'price',
         'old_price',
+        'discount',
     ];
 
     protected function casts(): array
@@ -30,9 +33,59 @@ class Product extends Model
         ];
     }
 
+
+    /**
+     *  Return the product seller
+     *
+     *  @return mixed
+     */
     public function getSeller() : mixed
     {
         return $this->belongsToMany(User::class, 'products_users', 'product_id', 'user_id')
             ->first();
+    }
+
+    /**
+     *  Return the product platform
+     *
+     *  @return Platform
+     */
+    public function getPlatform() : Platform
+    {
+        return $this->belongsTo(Platform::class, 'platform_id')
+            ->first();
+    }
+
+    /**
+     *  Return the product category
+     *
+     *  @return ProductCategory
+     */
+    public function getCategory(): ProductCategory
+    {
+        return $this->belongsTo(ProductCategory::class, 'category_id')
+            ->first();
+    }
+
+    /**
+     *  Return the product delivery
+     *
+     *  @return ProductDelivery
+     */
+    public function getDelivery() : ProductDelivery
+    {
+        return $this->belongsTo(ProductDelivery::class, 'delivery_id')
+            ->first();
+    }
+
+
+    /**
+     *  Return product discount in %
+     *
+     *  @return ?int
+     */
+    public function getDiscount() : ?int
+    {
+        return ProductController::getDiscount($this->price, $this->old_price);
     }
 }
