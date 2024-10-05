d5233c64f8a26f502f64f59115f65ea5d74bf82a
D0NIFF <abdulmalikgeraev59@gmail.com>
2024-10-05 09:52:59 +0300

    Add $fillable, $hidden, casts(), getType(), getProducts(IndexProductRequest $request)

--- app/Models/ProductService/Platform.php
+++ app/Models/ProductService/Platform.php
@@ -2,10 +2,60 @@
 
 namespace App\Models\ProductService;
 
+use App\Http\Requests\ProductService\IndexProductRequest;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
+use Illuminate\Database\Eloquent\Relations\HasMany;
 
 class Platform extends Model
 {
-    use HasFactory;
+
+    protected $fillable = [
+        'id',
+        'slug',
+        'title',
+        'img',
+        'banner',
+        'type_id',
+    ];
+
+    protected $hidden = [
+        'id',
+        'type_id',
+    ];
+
+    protected function casts(): array
+    {
+        return [
+            'slug' => 'string',
+            'title' => 'string',
+            'img' => 'string',
+            'banner' => 'string',
+        ];
+    }
+
+    /**
+     *  Return platform type
+     *
+     *  @return BelongsTo
+     */
+    public function getType(): BelongsTo
+    {
+        return $this->belongsTo(PlatformType::class, 'type_id');
+    }
+
+
+    /**
+     *  Return product with paginate
+     *
+     *  @param IndexProductRequest $request
+     *  @return mixed
+     */
+    public function getProducts(IndexProductRequest $request) : mixed
+    {
+        return $this->hasMany(Product::class, 'platform_id')
+            ->paginate((int) $request->get('perPage', 30), ['*'], 'page', (int) $request->get('page', 1));
+    }
+
 }
