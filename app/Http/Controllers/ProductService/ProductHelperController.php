40f4a562544771206c72ce9e8eaca3eff0fd22d2
D0NIFF <abdulmalikgeraev59@gmail.com>
2024-10-05 15:32:56 +0300

    Update method isUuid

--- app/Http/Controllers/ProductService/ProductHelperController.php
+++ app/Http/Controllers/ProductService/ProductHelperController.php
@@ -9,17 +9,19 @@ class ProductHelperController extends Controller
 {
     public static function isUuid(string $uuid, $version = null) : bool
     {
-        if ( ! is_string( $uuid ) ) {
+        if (!is_string($uuid))
             return false;
-        }
 
-        if ( is_numeric( $version ) ) {
+        if ( is_numeric( $version ) )
+        {
             if ( 4 !== (int) $version ) {
                 _doing_it_wrong( __FUNCTION__, __( 'Only UUID V4 is supported at this time.' ), '4.9.0' );
                 return false;
             }
             $regex = '/^[0-9a-f]{8}-[0-9a-f]{4}-4[0-9a-f]{3}-[89ab][0-9a-f]{3}-[0-9a-f]{12}$/';
-        } else {
+        }
+        else
+        {
             $regex = '/^[0-9a-f]{8}-[0-9a-f]{4}-[0-9a-f]{4}-[0-9a-f]{4}-[0-9a-f]{12}$/';
         }
 
