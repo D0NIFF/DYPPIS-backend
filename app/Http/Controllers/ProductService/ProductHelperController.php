32597802bd23fe01f54d36256962e79a8cf387d8
D0NIFF <abdulmalikgeraev59@gmail.com>
2024-10-05 16:13:46 +0300

    Move method getDiscount to ProductController

--- app/Http/Controllers/ProductService/ProductHelperController.php
+++ app/Http/Controllers/ProductService/ProductHelperController.php
@@ -26,9 +26,4 @@ class ProductHelperController extends Controller
     }
 
 
-    public static function getDiscount(int $price, int $oldPrice) : int
-    {
-        $percentValue = $price / 100; // Gets 1% of the price
-        return ($price - $oldPrice) / $percentValue;
-    }
 }
