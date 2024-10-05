0a0d839b8a56725bca3e975fbe79dc8058d9e2ef
D0NIFF <abdulmalikgeraev59@gmail.com>
2024-10-05 11:47:08 +0300

    Add PlatformResource and PlatformCollection

---
+++ app/Http/Resources/V1/ProductService/PlatformResource.php
@@ -0,0 +1,41 @@
+<?php
+
+namespace App\Http\Resources\V1\ProductService;
+
+use App\Http\Requests\ProductService\IndexProductRequest;
+use Illuminate\Http\Request;
+use Illuminate\Http\Resources\Json\JsonResource;
+
+class PlatformResource extends JsonResource
+{
+    /**
+     *  Transform the resource into an array.
+     *
+     *  @param bool $isCollection default false
+     *  @return array<string, mixed>
+     */
+    public function toArray(Request $request, bool $isCollection = false): array
+    {
+        if($isCollection)
+        {
+            return [
+                'slug' => $this->slug,
+                'title' => $this->title,
+                'img' => $this->img,
+                'banner' => $this->banner,
+                'categories' => ($this->getCategories()),
+            ];
+        }
+        else
+        {
+            return [
+                'slug' => $this->slug,
+                'title' => $this->title,
+                'img' => $this->img,
+                'banner' => $this->banner,
+                'products' => new ProductCollection($this->getProducts(IndexProductRequest::create($request))),
+                'categories' => ($this->getCategories()),
+            ];
+        }
+    }
+}
