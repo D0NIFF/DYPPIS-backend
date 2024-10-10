53f54aa48be06eebf385bf62a2903f175b4ce389
D0NIFF <abdulmalikgeraev59@gmail.com>
2024-10-10 04:54:35 +0300

    Add fieldExists, fieldUuid and fieldUnique

--- app/Http/Controllers/Api/V1/ErrorMessageController.php
+++ app/Http/Controllers/Api/V1/ErrorMessageController.php
@@ -16,25 +16,65 @@ class ErrorMessageController extends Controller
      */
     public static function fieldRequired(string $field) : array
     {
-        /* Get a word with translates */
-        $fields = json_decode(file_get_contents(config('app.texts') . '/fields.json'), true);
-        if(!isset($fields[$field]))
-        {
-            $fields = [
-                'en' => $field,
-                'es' => $field,
-                'fr' => $field,
-                'de' => $field,
-                'ru' => $field,
-            ];
-        }
-
         return [
-            'en' => 'The «' . $fields['en'] . '» field is required.',
-            'es' => 'El campo «' . $fields['es'] . '» es obligatorio.',
-            'de' => 'Das Feld «' . $fields['de'] . '» ist erforderlich.',
-            'fr' => 'Le champ «' . $fields['fr'] . '» est obligatoire.',
-            'ru' => 'Поле «' . $fields['ru'] . '» является обязательным.',
+            'en' => 'The «' . $field . '» field is required.',
+            'es' => 'El campo «' . $field . '» es obligatorio.',
+            'de' => 'Das Feld «' . $field . '» ist erforderlich.',
+            'fr' => 'Le champ «' . $field . '» est obligatoire.',
+            'ru' => 'Поле «' . $field . '» является обязательным.',
+        ];
+    }
+
+
+    /**
+     *  Return field exists message in few languages
+     *
+     *  @param string $field
+     *  @return string[]
+     */
+    public static function fieldExists(string $field) : array
+    {
+        return [
+            'en' => 'The selected «' . $field . '» is invalid..',
+            'es' => 'El id de la «' . $field . '» seleccionada no es válido.',
+            'de' => 'Die ausgewählte «' . $field . '» ist ungültig.',
+            'fr' => 'L\'identifiant de «' . $field . '» sélectionnée n\'est pas valide.',
+            'ru' => 'Выбранный «' . $field . '» недопустим.',
+        ];
+    }
+
+
+    /**
+     *  Return field exists message in few languages
+     *
+     *  @param string $field
+     *  @return string[]
+     */
+    public static function fieldUuid(string $field) : array
+    {
+        return [
+            'en' => 'The «' . $field . '» field must be a valid UUID.',
+            'es' => 'El campo «' . $field . '» debe ser un UUID válido.',
+            'de' => 'Das Feld «' . $field . '» muss eine gültige UUID sein.',
+            'fr' => 'Le champ «' . $field . '» doit être un UUID valide.',
+            'ru' => 'Поле «' . $field . '» должно быть действительным UUID.',
+        ];
+    }
+
+    /**
+     *  Return field unique message in few languages
+     *
+     *  @param string $field
+     *  @return string[]
+     */
+    public static function fieldUnique(string $field) : array
+    {
+        return [
+            'en' => 'The «' . $field . '» field must be a valid UUID.',
+            'es' => 'La «' . $field . '» ya ha sido tomada.',
+            'de' => 'Der «' . $field . '» ist bereits vergeben.',
+            'fr' => 'La «' . $field . '» a déjà été prise.',
+            'ru' => 'Поле «' . $field . '» уже занято.',
         ];
     }
 }
