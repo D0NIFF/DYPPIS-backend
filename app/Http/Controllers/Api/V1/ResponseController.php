775e4172accd9173c9addbd8d283b9ef9ce1a147
D0NIFF <abdulmalikgeraev59@gmail.com>
2024-10-10 18:33:51 +0300

    Create controller for responses

---
+++ app/Http/Controllers/Api/V1/ResponseController.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace App\Http\Controllers\Api\V1;
+
+use App\Http\Controllers\Controller;
+use Symfony\Component\HttpFoundation\JsonResponse;
+use Symfony\Component\HttpFoundation\Response;
+
+class ResponseController extends Controller
+{
+    //
+    public function index()
+    {
+
+    }
+
+    public static function success(array $data = [], int $code = 200) : JsonResponse
+    {
+        return new \Illuminate\Http\JsonResponse(
+            data: [
+                'code' => $code,
+                'status' => 'success',
+                'message' => 'Resource saved successfully.',
+                'data' => $data,
+            ],
+            status: Response::HTTP_OK
+        );
+    }
+}
