956f5c055e99689082eea6a88b04f84c9cdcc86f
D0NIFF <abdulmalikgeraev59@gmail.com>
2024-10-12 07:29:18 +0300

    add category_id

--- app/Models/MediaStorage.php
+++ app/Models/MediaStorage.php
@@ -1,16 +1,17 @@
 <?php
 
-namespace App\Models\ProductService;
+namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
 
-class ProductMediaStorage extends Model
+class MediaStorage extends Model
 {
     protected $fillable = [
         'id',
         'file_name',
         'file_type',
         'file_size',
+        'category_id',
         'created_at',
     ];
 
@@ -20,9 +21,14 @@ class ProductMediaStorage extends Model
             'file_name' => 'string',
             'file_type' => 'string',
             'file_size' => 'integer',
+            'category_id' => 'string',
             'created_at' => 'datetime',
         ];
     }
 
-
+    public function getCategory() : \DB
+    {
+        return \DB::table("media_storage_categories")->where('id', '=', $this['category_id'])
+            ->first();
+    }
 }
