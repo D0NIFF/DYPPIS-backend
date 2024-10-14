1119cddc223757a2551e2c2027d91a46bbfd9c2b
D0NIFF <abdulmalikgeraev59@gmail.com>
2024-10-14 18:20:17 +0300

    Create base controller for RESTful operations with the user

---
+++ app/Http/Controllers/Api/V1/UserService/UserApiController.php
@@ -0,0 +1,34 @@
+<?php
+
+namespace App\Http\Controllers\Api\V1\UserService;
+
+use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+
+class UserApiController extends Controller
+{
+    public function index(Request $request)
+    {
+
+    }
+
+    public function show(string $id)
+    {
+
+    }
+
+    public function store(Request $request)
+    {
+
+    }
+
+    public function update(Request $request, string $id)
+    {
+
+    }
+
+    public function destroy(string $id)
+    {
+
+    }
+}
