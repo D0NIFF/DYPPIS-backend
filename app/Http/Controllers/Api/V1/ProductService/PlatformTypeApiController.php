e74e289443827269b30458dc515f7eeb1c62e1b6
D0NIFF <abdulmalikgeraev59@gmail.com>
2024-10-17 07:43:49 +0300

    Method index return in Collection

--- app/Http/Controllers/Api/V1/ProductService/PlatformTypeApiController.php
+++ app/Http/Controllers/Api/V1/ProductService/PlatformTypeApiController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers\Api\V1\ProductService;
 
 use App\Http\Controllers\Controller;
+use App\Http\Resources\V1\ProductService\PlatformTypeCollection;
 use App\Http\Resources\V1\ProductService\ProductCollection;
 use App\Models\ProductService\PlatformType;
 use Illuminate\Http\Request;
@@ -18,16 +19,19 @@ class PlatformTypeApiController extends Controller
      *  @param string $field
      *  @return mixed
      */
-    public function index(string $field) : mixed
+    public function index(string $field = 'all') : mixed
     {
+        /* TODO: Remove cache forget after testing */
         if($field == 'all' || $field == null)
         {
+            Cache::forget('platform-types');
             return Cache::remember('platform-types', self::$secondsToDay * 365, function () {
-                return PlatformType::all();
+                return new PlatformTypeCollection(PlatformType::all());
             });
         }
         else
         {
+            Cache::forget('platform-types-' . $field);
             return Cache::remember('platform-types-' . $field, self::$secondsToDay * 365, function () {
                 return PlatformType::all()
                     ->pluck('slug')
