45d80e849efed569bfd70adf364a9802b68d0a4c
D0NIFF <abdulmalikgeraev59@gmail.com>
2024-10-17 07:43:27 +0300

    Initialize

--- app/Models/ProductService/PlatformType.php
+++ app/Models/ProductService/PlatformType.php
@@ -2,10 +2,36 @@
 
 namespace App\Models\ProductService;
 
-use Illuminate\Database\Eloquent\Factories\HasFactory;
+use App\Models\MediaStorage;
 use Illuminate\Database\Eloquent\Model;
 
 class PlatformType extends Model
 {
-    use HasFactory;
+    protected $fillable = [
+        'id',
+        'slug',
+        'title',
+        'image_id',
+    ];
+
+    protected $hidden = [
+        'id',
+        'image_id',
+    ];
+
+    protected function casts(): array
+    {
+        return [
+            'id' => 'string',
+            'slug' => 'string',
+            'title' => 'string',
+            'image_id' => 'string',
+        ];
+    }
+
+    public function getImage()
+    {
+        return $this->hasOne(MediaStorage::class, 'id', 'image_id')
+            ->first();
+    }
 }
