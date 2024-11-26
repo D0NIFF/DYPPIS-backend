543f878fc1da1447c3fce367aa0fdd751865a030
D0NIFF <abdulmalikgeraev59@gmail.com>
2024-11-26 19:31:01 +0300

    Refactor class method to static

--- app/Utils/ErrorMessages.php
+++ app/Utils/ErrorMessages.php
@@ -6,13 +6,33 @@ use Illuminate\Support\Facades\File;
 
 class ErrorMessages
 {
-    protected $messages;
+    protected static $messages;
 
-    public function __construct()
-    {
-        $path = resource_path('lang/error_messages.json');
-        $this->messages = json_decode(File::get($path), true);
-    }
+//    public function __construct()
+//    {
+//        $path = resource_path('lang/error_messages.json');
+//        self::$messages = json_decode(File::get($path), true);
+//    }
+//
+//    /**
+//     *  Get error messages for a given key and substitute the field
+//     *
+//     *  @param string $key
+//     *  @param string $field
+//     *  @return array
+//     */
+//    public function getMessages(string $key, string $field)
+//    {
+//        if (!isset($this->messages[$key])) {
+//            return ['en' => 'Message not found'];
+//        }
+//
+//        $response = array_map(function($message) use ($field) {
+//            return str_replace('{field}', $field, $message);
+//        }, $this->messages[$key]);
+//
+//        return $response;
+//    }
 
     /**
      *  Get error messages for a given key and substitute the field
@@ -21,15 +41,20 @@ class ErrorMessages
      *  @param string $field
      *  @return array
      */
-    public function getMessages(string $key, string $field)
+    public static function getMessages(string $key, string $field)
     {
-        if (!isset($this->messages[$key])) {
+        if (self::$messages === null) {
+            $path = resource_path('lang/error_messages.json');
+            self::$messages = json_decode(File::get($path), true);
+        }
+
+        if (!isset(self::$messages[$key])) {
             return ['en' => 'Message not found'];
         }
 
         $response = array_map(function($message) use ($field) {
             return str_replace('{field}', $field, $message);
-        }, $this->messages[$key]);
+        }, self::$messages[$key]);
 
         return $response;
     }
