8ac78b05b0246986e1c520a43a83ddb843b2ea61
D0NIFF <abdulmalikgeraev59@gmail.com>
2024-11-26 15:38:53 +0300

    Create a model for the platform

--- app/Models/ProductService/Platform.php
+++ app/Models/ProductService/Platform.php
@@ -5,12 +5,17 @@ namespace App\Models\ProductService;
 use App\Models\MediaStorage\MediaStorage;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Support\Facades\DB;
 
 class Platform extends Model
 {
     use HasFactory;
 
+    protected $keyType = 'string';
+    public $incrementing = false;
+
     protected $fillable = [
+        'id',
         'slug',
         'title',
         'image_id',
@@ -32,4 +37,10 @@ class Platform extends Model
     {
         return $this->belongsTo(PlatformType::class, 'type_id');
     }
+
+    public function categories() {
+        return $this->belongsToMany(ProductCategory::class, 'product_filters', 'platform_id', 'category_id');
+    }
+
+
 }
