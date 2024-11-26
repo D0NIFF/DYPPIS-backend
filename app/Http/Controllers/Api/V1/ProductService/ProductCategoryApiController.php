a3e672ddd6e7e6ec6d0c67908d4f8aaf1071c0bd
D0NIFF <abdulmalikgeraev59@gmail.com>
2024-11-26 19:36:44 +0300

    Create method index

--- app/Http/Controllers/Api/V1/ProductService/ProductCategoryApiController.php
+++ app/Http/Controllers/Api/V1/ProductService/ProductCategoryApiController.php
@@ -3,28 +3,50 @@
 namespace App\Http\Controllers\Api\V1\ProductService;
 
 use App\Http\Controllers\Controller;
+use App\Http\Resources\ProductService\ProductCategoryAllCollection;
+use App\Http\Resources\ProductService\ProductCategoryCollection;
+use App\Models\ProductService\Platform;
 use App\Models\ProductService\ProductCategory;
+use App\Utils\UuidHelper;
 use Illuminate\Http\Request;
 
 class ProductCategoryApiController extends Controller
 {
     /**
-     * Display a listing of the resource.
+     *  Display a listing of the resource.
+     *
+     *  @param Request $request
+     *  @param string|null $id
+     *  @return ProductCategoryAllCollection|ProductCategoryCollection
      */
-    public function index()
+    public function index(Request $request, string $id = null): ProductCategoryAllCollection|ProductCategoryCollection
     {
-        //
+        if($id)
+        {
+            $field = 'slug';
+            if(UuidHelper::isUuid($id))
+                $field = 'id';
+
+            $platform = Platform::where($field, $id)
+                ->firstOrFail();
+
+            $productCategories = $platform->categories;
+            return new ProductCategoryCollection($productCategories);
+        }
+        else
+        {
+            $productCategories = ProductCategory::paginate($request->get('perPage', 15));
+            return new ProductCategoryAllCollection($productCategories);
+        }
+
     }
 
     /**
      * Store a newly created resource in storage.
      */
-    public function store(Request $request, string $id = null)
+    public function store(Request $request)
     {
-        $productCategory = ProductCategory::query();
 
-        if($id)
-            $productCategory = $productCategory->where( $id);
     }
 
     /**
