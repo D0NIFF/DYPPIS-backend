12e04695087ace7b8ef8864b97052adaf8e67224
D0NIFF <abdulmalikgeraev59@gmail.com>
2024-11-27 19:29:50 +0300

    Refactor namespaces

--- app/Http/Requests/UserService/RegisterUserRequest.php
+++ app/Http/Requests/UserService/RegisterUserRequest.php
@@ -2,8 +2,8 @@
 
 namespace App\Http\Requests\UserService;
 
-use App\Http\Controllers\Api\V1\Response\RequestFormMessageController;
 use App\Utils\ErrorMessages;
+use Illuminate\Contracts\Validation\ValidationRule;
 use Illuminate\Foundation\Http\FormRequest;
 
 class RegisterUserRequest extends FormRequest
@@ -19,7 +19,7 @@ class RegisterUserRequest extends FormRequest
     /**
      * Get the validation rules that apply to the request.
      *
-     * @return array<string, \Illuminate\Contracts\Validation\ValidationRule|array<mixed>|string>
+     * @return array<string, ValidationRule|array|string>
      */
     public function rules(): array
     {
