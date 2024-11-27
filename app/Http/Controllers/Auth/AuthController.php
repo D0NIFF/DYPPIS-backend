bf4015455ebfad5a47ac0f0242780e3ddba8ed26
D0NIFF <abdulmalikgeraev59@gmail.com>
2024-11-27 19:25:46 +0300

    Refactor and renamed methods (registration, authorization)

--- app/Http/Controllers/Auth/AuthController.php
+++ app/Http/Controllers/Auth/AuthController.php
@@ -6,6 +6,7 @@ use App\Http\Controllers\Controller;
 use App\Http\Requests\UserService\LoginUserRequest;
 use App\Http\Requests\UserService\RegisterUserRequest;
 use App\Http\Resources\UserService\UserResource;
+use App\Utils\UuidHelper;
 use Illuminate\Http\JsonResponse;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Hash;
@@ -24,10 +25,12 @@ class AuthController extends Controller
      *  @param RegisterUserRequest $request
      *  @return JsonResponse
      */
-    public function register(RegisterUserRequest $request): JsonResponse
+    public function registration(RegisterUserRequest $request): JsonResponse
     {
-        $request->password = Hash::make($request->password);
-        $user = User::create($request);
+        $validatedData = $request->validated();
+        $validatedData['id'] = UuidHelper::generateUuid();
+        $validatedData['password'] = Hash::make($request->password);
+        $user = User::create($validatedData);
 
         $token = $user->createToken($request->device_name ?? 'undefined')->plainTextToken;
         return ApiResponse::created(['user' => new UserResource($user), 'token' => $token], 'User registered successfully.');
@@ -40,13 +43,13 @@ class AuthController extends Controller
      *  @return JsonResponse
      *  @throws ValidationException
      */
-    public function login(LoginUserRequest $request): JsonResponse
+    public function authorization(LoginUserRequest $request): JsonResponse
     {
         $user = User::where('email', $request->email)->first();
 
         if (!$user || !Hash::check($request->password, $user->password)) {
             throw ValidationException::withMessages([
-                'email' => ['The provided credentials are incorrect.'],
+                'credentials' => ['The provided credentials are incorrect.'],
             ]);
         }
 
