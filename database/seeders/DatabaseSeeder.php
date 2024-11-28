54b18c298aa403d0b618de48ad7ecc9bd6f8706d
D0NIFF <abdulmalikgeraev59@gmail.com>
2024-11-28 16:33:58 +0300

    Added the new seeders

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -2,11 +2,15 @@
 
 namespace Database\Seeders;
 
+use App\Models\ProductService\Product;
 use App\Models\User;
 // use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Database\Seeders\ProductService\PlatformSeeder;
 use Database\Seeders\ProductService\PlatformTypeSeeder;
 use Database\Seeders\ProductService\ProductCategorySeeder;
+use Database\Seeders\ProductService\ProductDeliverySeeder;
+use Database\Seeders\ProductService\ProductPictureSeeder;
+use Database\Seeders\ProductService\ProductResponseSeeder;
 use Illuminate\Database\Seeder;
 
 class DatabaseSeeder extends Seeder
@@ -21,6 +25,11 @@ class DatabaseSeeder extends Seeder
         $this->call(PlatformTypeSeeder::class);
         $this->call(PlatformSeeder::class);
         $this->call(ProductCategorySeeder::class);
+        $this->call(ProductDeliverySeeder::class);
+
+        $this->call(ProductResponseSeeder::class);
+        Product::factory(50)->create();
+        $this->call(ProductPictureSeeder::class);
 
     }
 }
