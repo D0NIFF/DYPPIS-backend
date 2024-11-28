006155a9e7ed76d45680d46dd5d7e0debb258974
D0NIFF <abdulmalikgeraev59@gmail.com>
2024-11-28 17:29:05 +0300

    Fixed image loads

--- database/seeders/ProductService/ProductPictureSeeder.php
+++ database/seeders/ProductService/ProductPictureSeeder.php
@@ -2,7 +2,9 @@
 
 namespace Database\Seeders\ProductService;
 
+use App\Models\MediaStorage\MediaStorage;
 use App\Models\ProductService\Product;
+use App\Utils\UuidHelper;
 use Illuminate\Database\Seeder;
 use Predis\Command\Traits\DB;
 
@@ -13,16 +15,36 @@ class ProductPictureSeeder extends Seeder
      */
     public function run(): void
     {
+        $images = [
+            [
+                'id' => '19626c29-d7da-56f9-88df-40dfde284a26',
+                'file_name' => '849f91a4-591a-4b6d-b3c3-679876dd3f47.jpg',
+                'file_type' => 'image/jpg',
+                'file_size' => 36864,
+                'category_id' => '56295c29-d7da-41e7-97df-40ffde284a93',
+            ],
+            [
+                'id' => '29626c29-d7da-56f9-88df-40dfde284a26',
+                'file_name' => '498db706-0276-4a4e-b551-6f2f0c0a6545.jpeg',
+                'file_type' => 'image/jpeg',
+                'file_size' => 36864,
+                'category_id' => '56295c29-d7da-41e7-97df-40ffde284a93',
+            ],
+        ];
+
+        MediaStorage::insert($images);
         $productIds = Product::all()->pluck('id')->toArray();
 
         foreach ($productIds as $productId) {
             \Illuminate\Support\Facades\DB::table('product_pictures')->insert([
+                'id' => UuidHelper::generateUuid(),
                 'product_id' => $productId,
-                'image_id' => '8fc30529-3329-4918-a836-15fda5d54b5a'
+                'image_id' => $images[0]['id'],
             ]);
             \Illuminate\Support\Facades\DB::table('product_pictures')->insert([
+                'id' => UuidHelper::generateUuid(),
                 'product_id' => $productId,
-                'image_id' => 'dae099e0-1518-425f-be75-5e7cc9fe3995'
+                'image_id' => $images[1]['id'],
             ]);
         }
     }
