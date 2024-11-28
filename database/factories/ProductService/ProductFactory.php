1eafd0c006777e0ceb6c3c2ea83f343e91d996fb
D0NIFF <abdulmalikgeraev59@gmail.com>
2024-11-28 16:34:29 +0300

    Update products factory

--- database/factories/ProductService/ProductFactory.php
+++ database/factories/ProductService/ProductFactory.php
@@ -21,7 +21,19 @@ class ProductFactory extends Factory
             'description' => $this->faker->paragraph(),
             'price' => $this->faker->numberBetween(1000, 10000),
             'old_price' => $this->faker->numberBetween(8000, 15000),
-
+            'response_id' => $this->faker->randomElement(['1f7f5u67-3b2f-4f5d-8c95-673cebh91632', '2f7f5u67-3b2f-4f5d-8c95-673cebh91632', '3f7f5u67-3b2f-4f5d-8c95-673cebh91632', '4f7f5u67-3b2f-4f5d-8c95-673cebh91632', '5f7f5u67-3b2f-4f5d-8c95-673cebh91632']),
+            'platform_id' => $this->faker->randomElement([
+                '1a8532ec-734a-4cab-bb52-9bc8bb2b4af8',
+                '2a8532ec-734a-4cab-bb52-9bc8bb2b4af8',
+                '3a8532ec-734a-4cab-bb52-9bc8bb2b4af8',
+                '4a8532ec-734a-4cab-bb52-9bc8bb2b4af8',
+                '5a8532ec-734a-4cab-bb52-9bc8bb2b4af8',
+                '6a8532ec-734a-4cab-bb52-9bc8bb2b4af8',
+                '7a8532ec-734a-4cab-bb52-9bc8bb2b4af8',
+                '8a8532ec-734a-4cab-bb52-9bc8bb2b4af8',
+            ]),
+            'category_id' => $this->faker->randomElement(['16236c66-d7da-56f9-92df-40dfde284b26', '26236c66-d7da-56f9-92df-40dfde284b26', '36236c66-d7da-56f9-92df-40dfde284b26', '46236c66-d7da-56f9-92df-40dfde284b26', '56236c66-d7da-56f9-92df-40dfde284b26']),
+            'delivery_id' => $this->faker->randomElement(['1e615962-37c0-3ab0-dbc8-e06b1681ddb0', '2e615962-37c0-3ab0-dbc8-e06b1681ddb0', '3e615962-37c0-3ab0-dbc8-e06b1681ddb0']),
         ];
     }
 }
