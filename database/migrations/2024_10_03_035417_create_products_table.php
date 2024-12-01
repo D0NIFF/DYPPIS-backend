a826cd3cc9531965a85f6ca5375dcffb98b1d4ce
D0NIFF <abdulmalikgeraev59@gmail.com>
2024-12-01 11:09:21 +0300

    Change a title type: string => text

--- database/migrations/2024_10_03_035417_create_products_table.php
+++ database/migrations/2024_10_03_035417_create_products_table.php
@@ -15,7 +15,7 @@ return new class extends Migration
             $table->uuid('id')
                 ->primary();
 
-            $table->string('title', 150)
+            $table->text('title')
                 ->index();
             $table->fullText('title', 'FULLTEXT_TITLE');
             $table->text('description')
